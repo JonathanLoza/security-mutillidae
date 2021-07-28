bfecefc9cb37d5229b4e851209bfd9522fc323cb
JonathanLoza <lozajnt@gmail.com>
2021-07-27 20:21:49 -0500

    remove comments

--- code/classes/SQLQueryHandler.php
+++ code/classes/SQLQueryHandler.php
@@ -344,13 +344,6 @@ class SQLQueryHandler {
 			$pUsername = $this->mMySQLHandler->escapeDangerousCharacters($pUsername);
 			$pPassword = $this->mMySQLHandler->escapeDangerousCharacters($pPassword);
 		}// end if
-
-		#$lQueryString =
-		#	"SELECT * FROM accounts
-		#	WHERE username='".$pUsername.
-		#	"' AND password='".$pPassword."'";
-
-		#return $this->mMySQLHandler->executeQuery($lQueryString);
 	
 		$lQueryString = "SELECT * FROM accounts WHERE username= ? AND password= ?";
 		return $this->mMySQLHandler->executeQuery_Prepare($lQueryString,$pUsername,$pPassword);
